f0f5180245ebef49cbf5ace55ca56eb44a21c0ea
Student <student@sirafrica.org>
2026-08-17 11:01:21 +0300

    8: fix eslint errors in index.ts

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -28,9 +28,8 @@ app.get('/bmi', (req, res) => {
 });
 
 app.post('/exercises', (req, res) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const body = req.body as any;
-  const { daily_exercises, target } = body;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
 
   if (daily_exercises === undefined || target === undefined) {
     return res.status(400).json({ error: 'parameters missing' });

f263fa9fe8f32af5a7e46ec18ac982a62a7b31c8
Student <student@sirafrica.org>
2026-08-20 09:27:37 +0300

    11: diagnoses endpoint

---
+++ patientor/backend/src/routes/diagnoses.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import type { Response } from 'express';
+import diagnosisService from '../services/diagnosisService.ts';
+import type { Diagnosis } from '../types.ts';
+
+const router = express.Router();
+
+router.get('/', (_req, res: Response<Diagnosis[]>) => {
+  res.send(diagnosisService.getEntries());
+});
+
+export default router;

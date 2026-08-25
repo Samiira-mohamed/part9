a41629f6e431aecce4ad3561bcd80c9bdf0b93e9
Student <student@sirafrica.org>
2026-08-25 09:53:16 +0300

    14: patient validation and type guards

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Response } from 'express';
 import patientService from '../services/patientService.ts';
 import type { NonSensitivePatient, Patient } from '../types.ts';
+import parseNewPatientEntry from '../utils.ts';
 
 const router = express.Router();
 
@@ -9,16 +10,18 @@ router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.send(patientService.getNonSensitiveEntries());
 });
 
-router.post('/', (req, res: Response<Patient>) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const addedPatient = patientService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
-  res.json(addedPatient);
+router.post('/', (req, res: Response<Patient | { error: string }>) => {
+  try {
+    const newPatientEntry = parseNewPatientEntry(req.body);
+    const addedPatient = patientService.addPatient(newPatientEntry);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send({ error: errorMessage });
+  }
 });
 
 export default router;
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -4,7 +4,13 @@ export interface Diagnosis {
   latin?: string;
 }
 
-export type Gender = 'male' | 'female' | 'other';
+export const Gender = {
+  Male: 'male',
+  Female: 'female',
+  Other: 'other',
+} as const;
+
+export type Gender = typeof Gender[keyof typeof Gender];
 
 export interface Patient {
   id: string;

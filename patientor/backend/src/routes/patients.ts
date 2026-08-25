7426ca279430c6b231d4dfb7e35ebafa1dfbf8c0
Student <student@sirafrica.org>
2026-08-25 11:57:29 +0300

    15: zod validation for patients

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,27 +1,46 @@
-import express from 'express';
-import type { Response } from 'express';
+import express, { type Request, type Response, type NextFunction } from 'express';
+import { z } from 'zod';
 import patientService from '../services/patientService.ts';
-import type { NonSensitivePatient, Patient } from '../types.ts';
-import parseNewPatientEntry from '../utils.ts';
+import { NewPatientSchema } from '../types.ts';
+import type { NonSensitivePatient, Patient, NewPatient } from '../types.ts';
 
 const router = express.Router();
+
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    NewPatientSchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
 
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.send(patientService.getNonSensitiveEntries());
 });
 
-router.post('/', (req, res: Response<Patient | { error: string }>) => {
-  try {
-    const newPatientEntry = parseNewPatientEntry(req.body);
-    const addedPatient = patientService.addPatient(newPatientEntry);
+router.post(
+  '/',
+  newPatientParser,
+  (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+    const addedPatient = patientService.addPatient(req.body);
     res.json(addedPatient);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send({ error: errorMessage });
   }
-});
+);
+
+const errorMiddleware = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.use(errorMiddleware);
 
 export default router;

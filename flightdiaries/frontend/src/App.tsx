ff27a6946e27828fb2665fba3f539600ec7bd906
Student <student@sirafrica.org>
2026-08-28 09:56:15 +0300

    20: add new diary entry form

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
-import type { DiaryEntry } from './types';
+import type { DiaryEntry, Weather, Visibility } from './types';
 import diaryService from './services/diaryService';
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState('');
+  const [weather, setWeather] = useState<Weather>('sunny');
+  const [visibility, setVisibility] = useState<Visibility>('great');
+  const [comment, setComment] = useState('');
 
   useEffect(() => {
     diaryService.getAll().then(data => {
@@ -11,9 +15,61 @@ const App = () => {
     });
   }, []);
 
+  const addDiary = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    diaryService
+      .create({ date, weather, visibility, comment })
+      .then(newDiary => {
+        setDiaries(diaries.concat(newDiary));
+        setDate('');
+        setWeather('sunny');
+        setVisibility('great');
+        setComment('');
+      });
+  };
+
   return (
     <div>
       <h1>Flight diaries</h1>
+
+      <h2>Add new entry</h2>
+      <form onSubmit={addDiary}>
+        <div>
+          date
+          <input
+            type="text"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div>
+          weather
+          <input
+            type="text"
+            value={weather}
+            onChange={(event) => setWeather(event.target.value as Weather)}
+          />
+        </div>
+        <div>
+          visibility
+          <input
+            type="text"
+            value={visibility}
+            onChange={(event) => setVisibility(event.target.value as Visibility)}
+          />
+        </div>
+        <div>
+          comment
+          <input
+            type="text"
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+
+      <h2>Diary entries</h2>
       {diaries.map(diary => (
         <div key={diary.id}>
           <h3>{diary.date}</h3>
--- flightdiaries/frontend/src/services/diaryService.ts
+++ flightdiaries/frontend/src/services/diaryService.ts
@@ -3,10 +3,18 @@ import type { DiaryEntry } from '../types';
 
 const baseUrl = 'http://localhost:3000/api/diaries';
 
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
+
 const getAll = () => {
   return axios
     .get<DiaryEntry[]>(baseUrl)
     .then(response => response.data);
 };
 
-export default { getAll };
+const create = (object: NewDiaryEntry) => {
+  return axios
+    .post<DiaryEntry>(baseUrl, object)
+    .then(response => response.data);
+};
+
+export default { getAll, create };
